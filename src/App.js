25ae6e02f83a76c6eac05e3e29ca703118eba139
prasadts <prasad@obaps.in>
2020-11-11 17:30:39 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [email, setEmail] = useState('');
+  function submitEmail() {
+    axios.post('http://localhost', {
+      email: email
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={(e) => setEmail(e.target.value)} />
+      <button onClick={submitEmail}>Submit</button>
     </div>
   );
 }
-
 export default App;
